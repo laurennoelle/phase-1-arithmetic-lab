49300603c76c424067a034ec82032c957aa998c8
Lauren Erxleben <lauren.n.erxleben@gmail.com>
2022-06-06 18:30:54 -0600

    commit tree 5fffc5e6c0e06a983332068eabedde756d5a4aac

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random() * 10);
+
+
+let num3 = 16;
+let num4 = 4;
+const mod = num3 / num4;
+
+const max = Math.max(5, 10, 15, 20);
